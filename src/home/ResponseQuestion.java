6870d7709b24b85905254d47cd93c1e271b6f6d8
Ramin Ahmari <rahmari@stanford.edu>
2016-03-09 20:48:30 -0800

    djbjbsbk

--- src/home/ResponseQuestion.java
+++ src/home/ResponseQuestion.java
@@ -1,10 +1,9 @@
 package home;
 
-import home.Question;
-
 public class ResponseQuestion extends Question {
 	public ResponseQuestion(String question, String answer) {
 		displayStr = question;
 		correctAnswer = answer;
+		type = "Response";
 	}
 }
